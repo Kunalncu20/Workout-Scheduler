635599b9711fd2ead2091589105f46b8609561b0
KunalMutreja <kunal20csu245@ncuindia.edu>
2023-02-24 15:36:22 +0530

    intial commit

--- server.js
+++ server.js
@@ -4,27 +4,27 @@ const express = require('express')
 //express app
 const app = express();
 
-const cors= require('cors');
+// const cors = require('cors');
 const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
 
-const workoutRoutes =require('./routes/workout')
+const workoutRoutes = require('./routes/workout')
 
-app.use(cors)
 //middleware
 app.use(express.json())
-app.use((req,res,next)=>{
-    console.log(req.path,req.method)
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
     next()
 })
- app.use('/api/workouts',workoutRoutes)
- 
- mongoose.connect(process.env.MONGO_URL)
- .then(()=>{
-     app.listen(process.env.PORT || 8080,()=>{
-         console.log("listening a port 4000!")
-     })
- }
- )
- .catch((error)=>{
-    console.log(error)
- })
+app.use('/api/workouts', workoutRoutes)
+// app.use(cors)
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log("listening a port "+process.env.PORT+"!")
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
